b2108cf5e4d712ec5f88daac8fdcf2c4b4bcaa61
Kavitha Srinivas <ksrinivs@us.ibm.com>
2014-09-17 16:46:17 -0400

    fix for various problems in load script

--- docker/load/runLoadPostgres.sh
+++ docker/load/runLoadPostgres.sh
@@ -1,6 +1,7 @@
+
 export PROCESSOR=`cat /proc/cpuinfo | grep 'processor' | wc -l`
 
-if [[$CREATE_DB=="true"]]; then
+if [[ x$CREATE_DB == "xtrue" ]]; then
     psql --command "CREATE USER quetzal WITH SUPERUSER PASSWORD 'quetzalcoatl';"
     createdb -O quetzal quetzal
 fi
