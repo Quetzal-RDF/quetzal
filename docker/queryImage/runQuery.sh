05ab58ff687701436975b6a5756feea93532ce10
Kavitha Srinivas <ksrinivs@us.ibm.com>
2014-10-02 09:32:22 -0400

    updated query

--- docker/queryImage/runQuery.sh
+++ docker/queryImage/runQuery.sh
@@ -1,9 +1,9 @@
 if [[ $1 == "LUBM" ]]; then
-    export QUERY_BASE=/sparqltosqlbase/test/lubm
+    export QUERY_BASE=/sparqltosqlbase/test/lubm/
     export KB_SIZE=$2
     ant Docker$3LUBM
 elif [[ $1 == "SP2B" ]]; then
-    export QUERY_BASE=/sparqltosqlbase/test/lubm
+    export QUERY_BASE=/sparqltosqlbase/test/lubm/
     export KB_SIZE=$2
     ant $2
     ant Docker$3LUBM
