d32727d5c09597d12752da097f09935873b4b748
Kavitha Srinivas <ksrinivs@us.ibm.com>
2014-10-01 22:00:20 -0400

    fixed errors in shell script

--- docker/queryImage/runQuery.sh
+++ docker/queryImage/runQuery.sh
@@ -2,11 +2,11 @@ if [[ $1 == "LUBM" ]]; then
     export QUERY_BASE=/sparqltosqlbase/test/lubm
     KB_SIZE=$2
     ant Docker$3LUBM
-elif [[ $1 == "SP2B" ]];
+elif [[ $1 == "SP2B" ]]; then
     export QUERY_BASE=/sparqltosqlbase/test/lubm
     KB_SIZE=$2
     ant $2
     ant Docker$3LUBM
-elif [[ x$1 == "--help" ]];
+elif [[ $1 == "--help" ]]; then
     echo "Usage: LUBM|SP2B <KB size: 100k, 10M, 100M> Postgresql|Shark"
 fi
