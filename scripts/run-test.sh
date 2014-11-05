09fc4f84d3f7c8a6f8cb26eeb817005dd19678bd
Julian Dolby <dolby@us.ibm.com>
2014-11-05 15:12:43 -0500

    new command line tool

--- scripts/run-test.sh
+++ scripts/run-test.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 DIR=`dirname $0`
-. $DIR/load-setup.sh
 
 TEST_CLASS=$1
-TEST_DIR=$2
+DATASET=$2
+ANSWERS=$3
+TEST_DIR=$4
 
+shift; shift; shift; shift
+
+. $DIR/load-setup.sh
 . $DB2_CONFIG
 
-java -Xmx1024M com.ibm.rdf.store.sparql11.$TEST_CLASS "jdbc:db2://$DB2_HOST:$DB2_PORT/$DB2_DB" $DB2_USER $DB2_PASSWORD $DB2_SCHEMA $TEST_DIR
+java -Xmx2048M com.ibm.rdf.store.sparql11.CommandLineDriver com.ibm.rdf.store.sparql11.$TEST_CLASS $DATASET $ANSWERS $TEST_DIR
+
 
