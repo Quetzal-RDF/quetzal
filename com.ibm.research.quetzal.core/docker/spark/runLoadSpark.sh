a570f21de8ed6aae54a33118045e88d010cec289
Kavitha Srinivas <ksrinivs@us.ibm.com>
2015-02-10 11:08:21 -0500

    more changes for docker

--- com.ibm.research.quetzal.core/docker/spark/runLoadSpark.sh
+++ com.ibm.research.quetzal.core/docker/spark/runLoadSpark.sh
@@ -7,7 +7,7 @@ chown -R quetzal /data
 /etc/bootstrap.sh
 
 /usr/local/spark/sbin/start-all.sh 
-/usr/local/spark/sbin/start-thriftserver.sh
+/usr/local/spark/sbin/start-thriftserver.sh &
 
 export PROCESSOR=`cat /proc/cpuinfo | grep 'processor' | wc -l`
 DIR=`dirname $0`
@@ -31,7 +31,7 @@ export DB2_PORT=10000
 export DB2_DB=default
 export DB2_USER=quetzal
 export DB2_PASSWORD=quetzalcoatl
-export DB2_SCHEMA=quetzal
+export DB2_SCHEMA=default
 export KNOWLEDGE_BASE=kb
 
 echo $FILETYPE
