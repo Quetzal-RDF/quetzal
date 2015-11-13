0d82373ba3258d455e669983da995f5166592af1
Kavitha Srinivas <ksrinivs@us.ibm.com>
2015-11-13 14:20:52 -0500

    fix for jdk8 dependency

--- com.ibm.research.quetzal.core/docker/db2/createDockerImage.sh
+++ com.ibm.research.quetzal.core/docker/db2/createDockerImage.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-git clone https://github.com/bryantsai/db2-docker.git
+git clone https://github.com/ksrinivs64/db2-docker.git
 
 cp $1 db2-docker/
 
 cd db2-docker
 
-sudo ./build.sh $1 
+sudo ./build.sh $1 server 1.0
 
 cd ..
 
