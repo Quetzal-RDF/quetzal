4d7649e827cba99685e9e135cf0a23ca4aca934b
Kavitha Srinivas <ksrinivs@us.ibm.com>
2016-01-08 15:29:05 -0500

    added apis for use cases

--- com.ibm.research.quetzal.core/pythonR/xpathTest.py
+++ com.ibm.research.quetzal.core/pythonR/xpathTest.py
@@ -24,9 +24,60 @@ def extractPost(funcData):
     result += "</data>"
     return result
 
+def extractSMILES():
+    root = etree.parse("../../drugbank.xml")
+    rows = root.xpath('/x:drugbank/x:drug', namespaces={'x': 'http://www.drugbank.ca'})
+    for row in rows:
+        print row.xpath('.//x:property[./x:kind/text()="SMILES"]/x:value/text()', namespaces={'x': 'http://www.drugbank.ca'})
+ #       print row.xpath('./x:calculated_properties/x:property[./x:kind/text()="SMILES"]/x:value/text()', namespaces={'x': 'http://www.drugbank.ca'})
+
+def extractSMILES(drugName):
+    root = etree.parse("../../drugbank.xml")
+    rows = root.xpath('/x:drugbank/x:drug[./x:name/text()="'+ drugName + '"]', namespaces={'x': 'http://www.drugbank.ca'})
+
+    result = '<?xml version="1.0"?>'
+    result += '<data xmlns="http://www.drugbank.ca">'
+
+    for row in rows:
+        smiles = row.xpath('.//x:property[./x:kind/text()="SMILES"]/x:value/text()', namespaces={'x': 'http://www.drugbank.ca'})
+        result += "<row>" + "<drug>" + drugName+ "</drug> <smiles>" + smiles[0] + "</smiles> </row>"
+    result += '</data>'
+    return result
+
+def extractDrugNames():
+    root = etree.parse("../../drugbank.xml")
+    rows = root.xpath('/x:drugbank/x:drug[./x:transporters/x:transporter/x:polypeptide/x:external-identifiers/x:external-identifier/x:resource/text()="UniProtKB"]', namespaces={'x': 'http://www.drugbank.ca'})
+
+    result = '<?xml version="1.0"?>'
+    result += '<data xmlns="http://www.drugbank.ca">'
+    for row in rows:
+        drug = row.xpath('./x:name/text()', namespaces={'x': 'http://www.drugbank.ca'})
+        result += "<row>" + "<drug>" + drug[0] + "</drug>  </row>"
+    result += '</data>'
+
+    return result
+
+
+def extractTransporters(drugName):
+    root = etree.parse("../../drugbank.xml")
+    rows = root.xpath('/x:drugbank/x:drug[./x:transporters/x:transporter/x:polypeptide/x:external-identifiers/x:external-identifier/x:resource/text()="UniProtKB"][./x:name/text()="'+ drugName + '"]', namespaces={'x': 'http://www.drugbank.ca'})
+
+    result = '<?xml version="1.0"?>'
+    result += '<data xmlns="http://www.drugbank.ca">'
+    for row in rows:
+        transporters = row.xpath('./x:transporters/x:transporter', namespaces={'x': 'http://www.drugbank.ca'})
+        for transporter in transporters:
+            actions = transporter.xpath('./x:actions/x:action', namespaces={'x': 'http://www.drugbank.ca'})
+            for action in actions:
+                act = action.xpath('./text()', namespaces={'x': 'http://www.drugbank.ca'})
+                id = transporter.xpath('./x:polypeptide/x:external-identifiers/x:external-identifier[./x:resource/text()="UniProtKB"]/x:identifier/text()', namespaces={'x': 'http://www.drugbank.ca'})
+                result += "<row>" + "<drug>" + drugName + "</drug> <id>" + id[0] + "</id> <action>" + act[0] + "</action> </row>"
+    result += '</data>'
+
+    return result
 
 def extractDrugBank():
-    root = etree.parse("drugbank.xml")
+    root = etree.parse("db_small.xml")
     rows = root.xpath('/x:drugbank/x:drug[./x:transporters/x:transporter/x:polypeptide/x:external-identifiers/x:external-identifier/x:resource/text()="UniProtKB"]', namespaces={'x': 'http://www.drugbank.ca'})
 
     result = '<?xml version="1.0"?>'
@@ -43,3 +94,7 @@ def extractDrugBank():
     result += '</data>'
 
     return result
+
+print extractSMILES('Ibuprofen')
+print extractTransporters('Ibuprofen')
+print extractDrugNames()
