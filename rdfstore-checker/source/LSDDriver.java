a1af8eb2fb313e91f75c54f4ce8992d0e910d2e2
Julian Dolby <juliandolby@gmail.com>
2018-04-05 13:33:31 -0400

    fixes for unsatisffiable cases, following the paper

--- rdfstore-checker/source/LSDDriver.java
+++ rdfstore-checker/source/LSDDriver.java
@@ -67,22 +67,49 @@ public class LSDDriver {
 		Formula f = Formula.TRUE;
 		Formula s1 = null;
 		Formula s2 = null;
-		Dataset dataset = DatasetFactory.createMem();
-		Graph G = dataset.asDatasetGraph().getDefaultGraph();
 		Set<Pair<Formula, Pair<Formula, Formula>>> fs = xlator.translateSingle(Collections.<String,Object>emptyMap(), true);
-		for(Pair<Formula, Pair<Formula, Formula>> p : fs) {
+		formulae: for(Pair<Formula, Pair<Formula, Formula>> p : fs) {
 			for(Relation r : ASTUtils.gatherRelations(p.fst)) {
 				if (r.name().equals("solution")) {
-					f = f.and(p.fst.and(r.some()));
-					s1 = s1==null? p.snd.fst: p.snd.fst==null? s1: p.snd.fst.and(s1);
-					s2 = s2==null? p.snd.snd: p.snd.snd==null? s2: p.snd.snd.and(s2);
+					
+					Formula thisf = p.fst.and(r.some());
+					if (Drivers.check(U, Pair.make(thisf, p.snd), "solution") == null) {
+						continue formulae;
+					}
+					
+					Formula nextf = f.and(thisf);
+					Formula nexts1 = s1==null? p.snd.fst: p.snd.fst==null? s1: p.snd.fst.and(s1);
+					Formula nexts2 = s2==null? p.snd.snd: p.snd.snd==null? s2: p.snd.snd.and(s2);
+					
+					TupleSet t = Drivers.check(U, Pair.make(nextf,  Pair.make(nexts1, nexts2)), "quads");
+					if (t == null) {
+						checkExpanded(ast, query, U, f, s1, s2);
+						f = thisf;
+						s1 = p.snd.fst;
+						s2 = p.snd.snd;
+					} else {
+						f = nextf;
+						s1 = nexts1;
+						s2 = nexts2;
+					}
+					
 					break;
 				}
 			}
 		}
 
 		System.err.println(f);
+		checkExpanded(ast, query, U, f, s1, s2);
+	}
+
+	private static void checkExpanded(Query ast, Op query, BasicUniverse U, Formula f, Formula s1, Formula s2)
+			throws URISyntaxException {
+		SolutionRelation s;
+		JenaTranslator xlator;
+		Pair<Formula, Pair<Formula, Formula>> xlation;
 		TupleSet t = Drivers.check(U, Pair.make(f,  Pair.make(s1, s2)), "quads");
+		Dataset dataset = DatasetFactory.createMem();
+		Graph G = dataset.asDatasetGraph().getDefaultGraph();
 		if (t != null) {
 			JenaUtil.addTupleSet(G, t);
 		}
@@ -109,7 +136,8 @@ public class LSDDriver {
 							@Override
 							public QueryTripleTerm get(Variable v) {
 								RDFNode val = row.get(v.getName());
-								return (QueryTripleTerm) val.visitWith(new RDFVisitor() {
+								if (val == null) return null;
+								else return (QueryTripleTerm) val.visitWith(new RDFVisitor() {
 									@Override
 									public Object visitBlank(Resource r, AnonId id) {
 										return new QueryTripleTerm(new BlankNodeVariable(id.getLabelString()));
@@ -158,6 +186,5 @@ public class LSDDriver {
 		xlation = xlator.translateSingle(Collections.<String,Object>emptyMap(), false).iterator().next();
 		System.err.println(xlation.fst);
 		Drivers.check(U, xlation, "solution");
-
 	}
 }
